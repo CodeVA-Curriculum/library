860d99d29088f6887ea53516de9b5451e659a28b
octojon <apollo@octojon.dev>
2026-09-04 12:11:38 -0400

    updated content

---
+++ src/pdfs.cjs
@@ -0,0 +1,90 @@
+var fs = require('fs')
+var env = require('env')
+request = require('request');
+var path = require('path')
+var os = require('os')
+const {google} = require('googleapis');
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+async function getIds() {
+    const res = await (await fetch('https://curriculum.codevirginia.org/api/library/all.json')).json()
+    let ids = []
+    for(const el of res) {
+        // only get the thumbnail if the file isn't a "group"
+        // if(el.links.drive == "https://docs.google.com/document/d/1CPYR86AiX5Oy6Ie53mzhAqwXwBhXHRLxWMY2pea1zBk/edit?usp=drive_link")
+        if(!el.contents && el.links.drive && el.links.drive.includes('/d/')) {
+            let id = el.links.drive.substring(el.links.drive.indexOf('/d/') + 3)
+            id = id.substring(0, id.indexOf('/'))
+            if(id.length > 0) {
+                ids.push(id)
+            }
+        }
+    }
+    return ids
+}
+
+async function getThumbnails(ids) {
+    for(const id of ids) {
+        console.log("downloading" + id)
+        download(`https://drive.google.com/thumbnail?sz=w640&id=${id}`, `./static/thumbnails/${id}.png`, function () {
+            console.log(`Got thumbnail for ${id}`)
+        })
+    }
+}
+
+async function getFile(id) {
+    download(`https://drive.google.com/thumbnail?sz=w640&id=${id}`, `./static/thumbnails/${id}.png`, function () {
+        console.log(`Got thumbnail for ${id}`)
+    })
+}
+
+async function main() {
+    const files = await getIds()
+    // for(const file of files) {
+    //     if(file == "1pJcVTPSp0IOIFhm4MMoWscCbPqP8arnM") {
+    //         console.log("PDF test passed!")
+    //     }
+    //     if( file == "1CPYR86AiX5Oy6Ie53mzhAqwXwBhXHRLxWMY2pea1zBk") {
+    //         console.log("Doc test passed!")
+    //     }
+    // }
+    // console.log(files)
+    // getThumbnails(files)
+    // getFile("11kOGV1Oh2A9tRXDm4lwY5cU41lamSAsHYFP3cIP73Sc")
+  //   const auth = new google.auth.GoogleAuth({
+  //   keyfilePath: path.join(__dirname, 'scripts/client_secret.json'),
+  //   scopes: 'https://www.googleapis.com/auth/drive.readonly',
+  // });
+  // const client = await auth.getClient();
+  // google.options({auth: client});
+  // 
+    const key = "AIzaSyCHanj0pjD7tyDOs-bDAD8wopzpf6DZyjw"
+    const drive = google.drive({version: 'v3', auth: key});
+    const destPath = path.join(os.tmpdir(), 'pdfs.zip');
+    const dest = fs.createWriteStream(destPath);
+    dest.setMaxListeners(files.length)
+    for(const fileId of files){
+        // const fileId = '1VWtbjiB2YOoxdt_QZPbp8ElImyyGczAlWkW70gbuL4M';
+        console.log("Downloading", fileId)
+        const res = await drive.files.export(
+          {fileId, mimeType: 'application/pdf'},
+          {responseType: 'stream'},
+        );
+        new Promise((resolve, reject) => {
+          res.data
+            .on('error', reject)
+            .pipe(dest)
+            .on('error', reject)
+            .on('finish', resolve);
+        });
+    }
+}
+
+main()
